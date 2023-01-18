781e932c594c6795b38f7b324c6a26b773c1ce6d
berger <robertberger.dev@gmail.com>
2023-01-18 14:27:57 -0500

    added code to index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
-function fullName(firstName: string, middleName: string, lastName: string): string
-{
-    return `The full name is ${firstName} ${middleName} ${lastName}.`;
+function fullName (firstName: string, middleName: string, lastName?: string): string
+{ 
+    if (lastName) {
+        return `The full name is ${firstName} ${middleName} ${lastName}.`;
+    } else {
+        return `The full name is ${firstName} ${middleName}.`;
+    }
 }
 
 console.log(fullName ("Bob", "Hawk", "Nelson")); //correct function call 
-console.log(fullName ("Bob", "Hawk")); // not enough parameters 
+console.log(fullName ("Bob", "Hawk")); // still works
 console.log(fullName ("Bob", "Hawk", "Nelson", "McGraw")); // too many parameters
