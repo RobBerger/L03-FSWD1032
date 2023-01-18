e752b495524bd1c8a8edd3fe6595f6e141dbdbe6
berger <robertberger.dev@gmail.com>
2023-01-18 14:28:36 -0500

    ran npm start

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 "use strict";
 function fullName(firstName, middleName, lastName) {
-    return `The full name is ${firstName} ${middleName} ${lastName}.`;
+    if (lastName) {
+        return `The full name is ${firstName} ${middleName} ${lastName}.`;
+    }
+    else {
+        return `The full name is ${firstName} ${middleName}.`;
+    }
 }
 console.log(fullName("Bob", "Hawk", "Nelson")); //correct function call 
-console.log(fullName("Bob", "Hawk")); // not enough parameters 
+console.log(fullName("Bob", "Hawk")); // still works
 console.log(fullName("Bob", "Hawk", "Nelson", "McGraw")); // too many parameters
