c34a91ef307219ac53cc98e61a453d3a01cc90d4
berger <robertberger.dev@gmail.com>
2023-01-18 14:31:37 -0500

    added rainman as defalt

--- index.ts
+++ index.ts
@@ -1,12 +1,8 @@
-function fullName (firstName: string, middleName: string, lastName?: string): string
+function fullName (firstName: string, middleName: string, lastName = "Rainman")
 { 
-    if (lastName) {
-        return `The full name is ${firstName} ${middleName} ${lastName}.`;
-    } else {
-        return `The full name is ${firstName} ${middleName}.`;
-    }
+    return `The full name is ${firstName} ${middleName} ${lastName}.`;
 }
 
 console.log(fullName ("Bob", "Hawk", "Nelson")); //correct function call 
-console.log(fullName ("Bob", "Hawk")); // still works
+console.log(fullName ("Bob", "Hawk")); // still works, but will now return "The full name is Bob Hawk Rainman"
 console.log(fullName ("Bob", "Hawk", "Nelson", "McGraw")); // too many parameters
